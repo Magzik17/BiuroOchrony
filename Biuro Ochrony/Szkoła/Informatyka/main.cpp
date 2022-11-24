28f0323119408ae6e9a18fad0156b097ca96f45c
Magzik17 <69162994+Magzik17@users.noreply.github.com>
2022-11-24 18:04:37 +0100

    Add files via upload

---
+++ Biuro Ochrony/Szkoła/Informatyka/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+int main(int argc, char *argv[])
+{
+	system("chcp 1250");
+ printf("jbnb");
+ cout<<endl;
+   printf("Teraz kasujê ekran i dalsza czeœæ programu po naciœniêciu klawisza Enter");
+   cout<<endl;
+ system("PAUSE");
+ printf("ghvvvvv");
+ cout<<endl;
+    printf("Teraz kasujê ekran i dalsza czeœæ programu po naciœniêciu klawisza Enter");
+   cout<<endl;
+ system("PAUSE");
+  printf("ghvvvvv");
+ cout<<endl;
+    printf("Teraz kasujê ekran i dalsza czeœæ programu po naciœniêciu klawisza Enter");
+   cout<<endl;
+ system("PAUSE");
+  printf("ghvvvvv");
+ cout<<endl;
+    printf("Teraz kasujê ekran i dalsza czeœæ programu po naciœniêciu klawisza Enter");
+   cout<<endl;
+ system("PAUSE");
+  printf("ghvvvvv");
+ cout<<endl;
+ system("PAUSE");
+ retur]\n 0;
+}
